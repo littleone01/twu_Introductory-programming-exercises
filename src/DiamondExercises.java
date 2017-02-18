7cbf150473e50b3d273c0c49c2cc2466cb5898c3
yibwang <yibwang@thoughtworks.com>
2017-02-18 12:26:39 +0800

    Finish Diamond Exercises.

--- src/DiamondExercises.java
+++ src/DiamondExercises.java
@@ -2,9 +2,20 @@
  * Created by yibwang on 2/18/17.
  */
 public class DiamondExercises {
-    private void IsoscelesTriangle(int count) {
+    private void isoscelesTriangle(int count) {
         int length = count * 2 - 1;
-        for (int i = 0; i < count; i ++) {
+        drawTriangle(length, count);
+    }
+
+    private void drawTriangle(int length, int height) {
+        for (int i = 0; i < height; i ++) {
+            int asteriskLength = i * 2 + 1;
+            drawLineInCenter(asteriskLength, length);
+        }
+    }
+
+    private void reverseTriangleInDiamond(int length, int count) {
+        for (int i = count - 1; i >= 0; i --) {
             int asteriskLength = i * 2 + 1;
             drawLineInCenter(asteriskLength, length);
         }
@@ -22,8 +33,23 @@ public class DiamondExercises {
         System.out.println(line);
     }
 
+    private void drawDiamond(int count) {
+        int length = count * 2 - 1;
+        drawTriangle(length, count);
+        reverseTriangleInDiamond(length, count - 1);
+    }
+
+    private void drawDiamondWithName (int count, String name) {
+        int length = count * 2 - 1;
+        drawTriangle(length, count - 1);
+        System.out.println(name);
+        reverseTriangleInDiamond(length, count - 1);
+    }
+
     public static void main(String[] args) {
         DiamondExercises exercises = new DiamondExercises();
-        exercises.IsoscelesTriangle(3);
+        exercises.isoscelesTriangle(3);
+        exercises.drawDiamond(3);
+        exercises.drawDiamondWithName(3, "Bill");
     }
 }
